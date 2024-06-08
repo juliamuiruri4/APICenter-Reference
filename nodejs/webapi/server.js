b5b33ffb25da1074cecfaeb3759ddf0838d42c6f
Justin Yoo <justin.yoojh@gmail.com>
2024-06-08 12:23:59 +0900

    Add some suggestions

--- nodejs/webapi/server.js
+++ nodejs/webapi/server.js
@@ -1,7 +1,11 @@
-const express = require('express'); // import express
-swaggerJSDoc = require('swagger-jsdoc'); // import swagger-jsdoc to generate swagger doc
-swaggerUi = require('swagger-ui-express'); // import swagger-ui-express to serve swagger docs
-const { swaggerSpecs, setupSwaggerUi } = require('./swagger/openapiservice');
+const express = require("express"); // import express
+swaggerJSDoc = require("swagger-jsdoc"); // import swagger-jsdoc to generate swagger doc
+swaggerUi = require("swagger-ui-express"); // import swagger-ui-express to serve swagger docs
+const {
+  server,
+  swaggerSpecs,
+  setupSwaggerUi,
+} = require("./swagger/openapiservice");
 
 // JSDoc comments for api endpoints
 /**
@@ -39,16 +43,18 @@ const { swaggerSpecs, setupSwaggerUi } = require('./swagger/openapiservice');
  *            type: integer
  *            description: Temperature in Fahrenheit
  *            example: 123
-*/
-
+ */
 const app = express();
 
 // Random weather data
 const generateRandomTemperature = () => Math.floor(Math.random() * 61) - 20;
-const generateRandomSummary = () => ['Hot', 'Warm', 'Mild', 'Cool', 'Bracing', 'Scorching', 'Freezing'][Math.floor(Math.random() * 7)];
+const generateRandomSummary = () =>
+  ["Hot", "Warm", "Mild", "Cool", "Bracing", "Scorching", "Freezing"][
+    Math.floor(Math.random() * 7)
+  ];
 
 // Function to generate random weather data for a specified number of days
-const generateRandomWeatherData = numDays => {
+const generateRandomWeatherData = (numDays) => {
   const startDate = new Date();
   return Array.from({ length: numDays }, (_, i) => {
     const date = new Date(startDate);
@@ -56,40 +62,55 @@ const generateRandomWeatherData = numDays => {
     const temperatureC = generateRandomTemperature();
     const temperatureF = Math.round((temperatureC * 9) / 5 + 32);
     const summary = generateRandomSummary();
-    return { date: date.toISOString().split('T')[0], temperatureC, summary, temperatureF };
+    return {
+      date: date.toISOString().split("T")[0],
+      temperatureC,
+      summary,
+      temperatureF,
+    };
   });
 };
 
-// Generate random weather data for the next 5 days
-const weatherData = generateRandomWeatherData(5);
-
-// send weather data to /weatherdata
-app.get('/weatherforecast', (req, res) => {
-    res.json(weatherData);
-});
-
 // route to serve basic swagger JSON, Set up Swagger UI and redirect
-// app.get('/swagger.json', (req, res) => {
-//     res.setHeader('Content-Type', 'application/json');
-//     res.send(swaggerSpecs.basic);
-// });
-// setupSwaggerUi(app, swaggerSpecs.basic, '/api-docs');
-// app.get('/', (req, res) => {
-//   res.redirect('/api-docs');
-// });
-
+app.get(
+  `${server.basic.basePath.replace(/\/$/, "")}/weatherforecast`,
+  (req, res) => {
+    // Generate random weather data for the next 5 days
+    const weatherData = generateRandomWeatherData(5);
+    res.setHeader("Content-Type", "application/json");
+    res.json(weatherData);
+  }
+);
+app.get("/", (req, res) => {
+  res.redirect(`${server.basic.basePath.replace(/\/$/, "")}/weatherforecast`);
+});
+setupSwaggerUi(app, swaggerSpecs.basic, "/swagger");
+app.get("/api-docs/swagger.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerSpecs.basic);
+});
 
 // route to serve improved swagger JSON, Set up Swagger UI and redirect
-app.get('/swagger.json', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(swaggerSpecs.improved);
-});
-setupSwaggerUi(app, swaggerSpecs.improved, '/api-docs');
-app.get('/', (req, res) => {
-  res.redirect('/api-docs');
-});
+// app.get(
+//   `${server.improved.basePath.replace(/\/$/, "")}/weatherforecast`,
+//   (req, res) => {
+//     // Generate random weather data for the next 5 days
+//     const weatherData = generateRandomWeatherData(5);
+//     res.setHeader("Content-Type", "application/json");
+//     res.json(weatherData);
+//   }
+// );
+// app.get("/", (req, res) => {
+//   res.redirect(
+//     `${server.improved.basePath.replace(/\/$/, "")}/weatherforecast`
+//   );
+// });
+// setupSwaggerUi(app, swaggerSpecs.improved, "/swagger");
+// app.get("/api-docs/swagger.json", (req, res) => {
+//   res.setHeader("Content-Type", "application/json");
+//   res.send(swaggerSpecs.improved);
+// });
 
-const port = process.env.PORT || 3030;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(server.port, () => {
+  console.log(`Server running on port ${server.port}`);
 });
--- nodejs/webapi/swagger/openapiservice.js
+++ nodejs/webapi/swagger/openapiservice.js
@@ -1,58 +1,77 @@
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const port = process.env.PORT || 8080;
+const server = {
+  port: port,
+  basic: {
+    basePath: "/",
+  },
+  improved: {
+    basePath: "/api",
+  },
+};
 
 // Basic swagger definition
 const BasicswaggerDefinition = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-         title: "ApiApp",
-         description: "[http://localhost:3030/swagger.json](http://localhost:3030/swagger.json)",
-         version: "1.0.0"
-        },
+  definition: {
+    openapi: "3.0.1",
+    info: {
+      title: "webapi",
+      description: `[http://localhost:${port}/api-docs/swagger.json](http://localhost:${port}/api-docs/swagger.json)`,
+      version: "1.0.0",
     },
-    apis: ['./server.js'],
+  },
+  apis: ["./server.js"],
 };
 
 // Improved swagger definition
 const ImprovedswaggerDefinition = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-         title: "Weather Forecast API",
-         description: "[http://localhost:3030/swagger.json](http://localhost:3030/swagger.json)",
-         version: "1.0.0",
-         contact: {
-            name: 'Contoso',
-            email: 'api@contoso.com',
-            url: 'https://contoso.com/support',
-         },
-        },
-        servers: [
-           {
-            url: 'http://localhost:3030',
-           },
-        ],
-        tags: [
-           {
-            name: 'Weather',
-            description: 'Weather API',
-           },
-        ],
+  definition: {
+    openapi: "3.0.1",
+    info: {
+      title: "Weather Forecast API",
+      description: `[http://localhost:${port}/api-docs/swagger.json](http://localhost:${port}/api-docs/swagger.json)<br>An API that predicts upcoming weather`,
+      version: "v1.0.0",
+      contact: {
+        name: "Contoso",
+        email: "api@contoso.com",
+        url: "https://contoso.com/support",
+      },
     },
-    apis: ['./server.js'],
-  };
+    servers: [
+      {
+        url: `http://localhost:${port}/api`,
+      },
+    ],
+    tags: [
+      {
+        name: "Weather",
+        description: "Weather forecast operations",
+      },
+    ],
+  },
+  apis: ["./server.js"],
+};
 
 const swaggerSpecs = {
   basic: swaggerJsdoc(BasicswaggerDefinition),
   improved: swaggerJsdoc(ImprovedswaggerDefinition),
 };
 
-const setupSwaggerUi = (app, specs, path = '/api-docs') => {
-  app.use(path, swaggerUi.serve, swaggerUi.setup(specs));
+const options = {
+  explorer: true,
+  swaggerOptions: {
+    url: "/api-docs/swagger.json",
+  },
+};
+
+const setupSwaggerUi = (app, specs, path = "/api-docs") => {
+  app.use(path, swaggerUi.serve, swaggerUi.setup(specs, options));
 };
 
 module.exports = {
+  server,
   swaggerSpecs,
   setupSwaggerUi,
 };
